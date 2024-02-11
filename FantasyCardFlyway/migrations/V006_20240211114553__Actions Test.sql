dcdff498f3e3eb54916862abce69f875c67c0a0e
Johan Viljoen <johan.viljoen@bbd.co.za>
2024-02-11 13:55:04 +0200

    Test run for github actions

---
+++ FantasyCardFlyway/migrations/V006_20240211114553__Actions Test.sql
@@ -0,0 +1,13 @@
+
+USE FantasyTCStore
+GO
+
+CREATE TABLE dbo.Person
+create table PERSON (
+ID int not null,
+NAME varchar(100) not null
+);
+
+INSERT INTO PERSON (ID, NAME)
+VALUES (1, 'John'),
+       (2, 'Jane');
