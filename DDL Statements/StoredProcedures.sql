29b585252ca855be88b9b710510820b9ce8652f1
janro13806 <80365036+janro13806@users.noreply.github.com>
2024-02-13 10:36:35 +0200

    Update The Add Transaction line function
    > CheckValidTransaction function as an extra check
    > UpdateStockCount procedure to either increase or decrease the count depending on the transaction type.

--- DDL Statements/StoredProcedures.sql
+++ DDL Statements/StoredProcedures.sql
@@ -30,13 +30,34 @@ GO
 CREATE PROCEDURE AddTransactionLine
     @CardID INT,
     @TransactionID INT,
-    @Quantity INT
+    @Quantity INT,
+    @TransactionTypeID INT
 AS
 BEGIN
-    INSERT INTO TransactionLine (CardID, TransactionID, Quantity)
-    VALUES (@CardID, @TransactionID, @Quantity)
+    DECLARE @ValidTransaction BIT;
+    DECLARE @NewStockQty INT;
+    
+    SET @ValidTransaction = dbo.CheckValidTransaction(@CardID, @Quantity, @TransactionTypeID);
+    
+    IF @ValidTransaction = 1
+    BEGIN
+        INSERT INTO TransactionLine (StockID, TransactionID, Quantity)
+        VALUES (@CardID, @TransactionID, @Quantity);
+        
+        IF @TransactionTypeID = 1 -- Buy transaction
+            SET @NewStockQty = (SELECT QuantityInStock + @Quantity FROM Stock WHERE StockID = @CardID);
+        ELSE -- Sell transaction
+            SET @NewStockQty = (SELECT QuantityInStock - @Quantity FROM Stock WHERE StockID = @CardID);
+
+        EXEC UpdateStockCount @StockId = @CardID, @NewQty = @NewStockQty;
+    END
+    ELSE
+    BEGIN
+        RAISERROR ('Invalid transaction. Please check the quantity and availability of the card.', 16, 1);
+    END
 END;
 GO
+
 
 CREATE PROCEDURE UpdateStockCount
     @StockId INT,
